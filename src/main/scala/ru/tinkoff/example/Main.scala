fa6d2ee062d115550ac223a96e8cf7ed08dbf060
enp0s23 <enp0s23@gmail.com>
2019-04-09 17:19:15 +0700

    Добавил logback и настроил деплой в heroku

--- src/main/scala/ru/tinkoff/example/Main.scala
+++ src/main/scala/ru/tinkoff/example/Main.scala
@@ -1,17 +1,17 @@
 package ru.tinkoff.example
 
 import scala.concurrent.Future
-
 import cats.instances.future._
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import cats.arrow.FunctionK
-
 import ru.tinkoff.example.sd.access.{AsyncJdbcAppAccess, SyncJdbcAppAccess}
 import ru.tinkoff.example.sd.service.SDServiceImpl
 import ru.tinkoff.example.web.Server
 import ru.tinkoff.example.web.sd.SDModule
+
+import scala.util.Try
 
 object Main extends App {
 
@@ -26,5 +26,11 @@ object Main extends App {
   val server = new Server(module)
 
   SyncJdbcAppAccess.initSchema()
-  Http().bindAndHandle(server.routes, "0.0.0.0", 9991)
+
+  val port = (for {
+    rawPort <- Option(System.getenv("PORT"))
+    portFromEnv <- Try(rawPort.toInt).toOption
+  } yield portFromEnv) getOrElse 9991
+
+  Http().bindAndHandle(server.routes, "0.0.0.0", port)
 }
